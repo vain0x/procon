95a56fc162c072aadf1161aa95067dc2bab5b655
vain0x <vainzerox@gmail.com>
2017-06-10 01:15:19 +0900

    Set up

--- visual-studio-csharp/VainZero.Procon.CSharp/Program.cs
+++ visual-studio-csharp/VainZero.Procon.CSharp/Program.cs
@@ -1,17 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 public class Program
 {
+    #region Standard I/O
+    readonly TextReader input;
+    readonly TextWriter output;
+
+    X[] ReadLine<X>(Func<string, X> func)
+    {
+        return input.ReadLine().Split(' ').Select(func).ToArray();
+    }
+
+    void WriteLineOne(object obj)
+    {
+        output.WriteLine("{0}", obj);
+    }
+
+    void WriteLineMany(params object[] objects)
+    {
+        output.WriteLine(string.Join(" ", objects.Select(obj => obj.ToString())));
+    }
+    #endregion
+
     public void Run()
     {
     }
 
+    public Program(TextReader input, TextWriter output)
+    {
+        this.input = input;
+        this.output = output;
+    }
+
     public static void Main(string[] args)
     {
-        new Program().Run();
+        new Program(Console.In, Console.Out).Run();
     }
 }
