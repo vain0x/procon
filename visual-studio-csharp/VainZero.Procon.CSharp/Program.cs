ebd06493d84e6a7b713cde3469cfd1295fb38879
vain0x <vainzerox@gmail.com>
2017-06-10 00:18:11 +0900

    Add solution

---
+++ visual-studio-csharp/VainZero.Procon.CSharp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Program
+{
+    public void Run()
+    {
+    }
+
+    public static void Main(string[] args)
+    {
+        new Program().Run();
+    }
+}
