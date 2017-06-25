cc20ff62db5e0c92ecf6ade603212136d1409588
vain0x <vainzerox@gmail.com>
2017-06-25 18:07:43 +0900

    Add UnionFindForest

---
+++ visual-studio-csharp/VainZero.Procon.CSharp.Library.Tests/Collections/UnionFindForestTest.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace VainZero.Collections
+{
+    public sealed class UnionFindForestTest
+    {
+        [Fact]
+        public void Test_initial_state()
+        {
+            var n = 10;
+            var uff = new UnionFindForest(n);
+            for (var i = 0; i < n; i++)
+            {
+                Assert.Equal(i, uff.Root(i));
+            }
+        }
+
+        [Fact]
+        public void Test_single_group_case()
+        {
+            var n = 10;
+
+            var uff = new UnionFindForest(n);
+            for (var i = 0; i + 1 < n; i++)
+            {
+                uff.Merge(i, i + 1);
+            }
+
+            var r = uff.Root(0);
+
+            for (var i = 1; i < n; i++)
+            {
+                Assert.Equal(r, uff.Root(i));
+                Assert.True(uff.Connects(r, uff.Root(i)));
+            }
+        }
+
+        [Fact]
+        public void Test_two_group_case()
+        {
+            var n = 10;
+
+            var uff = new UnionFindForest(n);
+            for (var i = 0; i < n; i++)
+            {
+                uff.Merge(i, i % 2);
+            }
+
+            Assert.Equal(0, uff.Root(0) % 2);
+            Assert.Equal(1, uff.Root(1) % 2);
+
+            for (var i = 0; i < n; i++)
+            {
+                Assert.Equal(uff.Root(i % 2), uff.Root(i));
+                Assert.True(uff.Connects(i % 2, i));
+            }
+        }
+    }
+}
---
+++ visual-studio-csharp/VainZero.Procon.CSharp.Library/Collections/UnionFindForest.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VainZero.Collections
+{
+    public sealed class UnionFindForest
+    {
+        readonly int[] parents;
+        readonly int[] ranks;
+
+        /// <summary>
+        /// Constructs n-vertex UFF.
+        /// </summary>
+        public UnionFindForest(int n)
+        {
+            parents = new int[n];
+            ranks = new int[n];
+
+            for (var v = 0; v < n; v++)
+            {
+                parents[v] = v;
+                ranks[v] = 1;
+            }
+        }
+
+        /// <summary>
+        /// Gets the representative of the specified vertex's group.
+        /// </summary>
+        public int Root(int v)
+        {
+            if (parents[v] == v)
+            {
+                return v;
+            }
+
+            var r = Root(parents[v]);
+            parents[v] = r;
+            return r;
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether two vertices belong to the same group.
+        /// </summary>
+        public bool Connects(int u, int v)
+        {
+            return Root(u) == Root(v);
+        }
+
+        static void Swap<X>(ref X l, ref X r)
+        {
+            var t = l;
+            l = r;
+            r = t;
+        }
+
+        /// <summary>
+        /// Merges the specified vertices' group.
+        /// </summary>
+        public void Merge(int u, int v)
+        {
+            u = Root(u);
+            v = Root(v);
+            if (u == v) return;
+
+            if (ranks[u] > ranks[v])
+            {
+                Swap(ref u, ref v);
+            }
+
+            parents[u] = v;
+            ranks[v] += ranks[u];
+        }
+    }
+}
