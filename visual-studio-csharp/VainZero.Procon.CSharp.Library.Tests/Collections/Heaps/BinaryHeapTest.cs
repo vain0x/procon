2120ccf63c30f129850e4e3a4b2830084513b094
vain0x <vainzerox@gmail.com>
2017-06-25 17:42:27 +0900

    Add BinaryHeap

---
+++ visual-studio-csharp/VainZero.Procon.CSharp.Library.Tests/Collections/Heaps/BinaryHeapTest.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace VainZero.Collections.Heaps
+{
+    public sealed class BinaryHeapTest
+    {
+        static IEnumerable<int> UnorderedSeq(int n)
+        {
+            return Enumerable.Range(0, n).Select(i => (i * i * i) % 17);
+        }
+
+        [Fact]
+        public void Test_Enqueue_Dequeue()
+        {
+            var heap = BinaryHeap.Create<int>();
+
+            Assert.Equal(0, heap.Count);
+            Assert.ThrowsAny<Exception>(() => heap.Dequeue());
+            Assert.ThrowsAny<Exception>(() => heap.Peek());
+
+            heap.Enqueue(1);
+            Assert.Equal(1, heap.Count);
+            Assert.Equal(1, heap.Peek());
+
+            // Test to add a value larger than min.
+            {
+                heap.Enqueue(2);
+                Assert.Equal(2, heap.Count);
+                Assert.Equal(1, heap.Peek());
+
+                var x = heap.Dequeue();
+                Assert.Equal(1, heap.Count);
+                Assert.Equal(1, x);
+            }
+
+            // Test to add a value less than min.
+            {
+                heap.Enqueue(0);
+                Assert.Equal(2, heap.Count);
+                Assert.Equal(0, heap.Peek());
+
+                var x = heap.Dequeue();
+                Assert.Equal(1, heap.Count);
+                Assert.Equal(0, x);
+            }
+        }
+
+        [Fact]
+        public void Test_GetEnumerator()
+        {
+            var n = 100;
+
+            var heap = BinaryHeap.Create<int>();
+            var xs = UnorderedSeq(n).ToArray();
+
+            foreach (var x in xs)
+            {
+                heap.Enqueue(x);
+            }
+
+            var actual = heap.OrderBy(x => x);
+
+            Array.Sort(xs);
+            Assert.Equal(xs, actual);
+        }
+
+        [Fact]
+        public void Test_FromEnumerable()
+        {
+            var n = 10;
+
+            var xs = UnorderedSeq(n).ToArray();
+            var heap = BinaryHeap.FromEnumerable(xs);
+
+            var ys = new List<int>();
+            while (heap.Count > 0)
+            {
+                ys.Add(heap.Dequeue());
+            }
+
+            Array.Sort(xs);
+            Assert.Equal(xs, ys);
+        }
+    }
+}
---
+++ visual-studio-csharp/VainZero.Procon.CSharp.Library/Collections/Heaps/BinaryHeap.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace VainZero.Collections.Heaps
+{
+    public sealed class BinaryHeap<TValue>
+        : IReadOnlyCollection<TValue>
+    {
+        readonly List<TValue> list;
+        readonly Func<TValue, TValue, int> compare;
+
+        public int Count
+        {
+            get { return list.Count; }
+        }
+
+        public IEnumerator<TValue> GetEnumerator()
+        {
+            return list.GetEnumerator();
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public TValue Peek()
+        {
+            return list[0];
+        }
+
+        public void Enqueue(TValue value)
+        {
+            list.Add(value);
+            var i = list.Count - 1;
+            while (i > 0)
+            {
+                // Index of the parent.
+                var p = (i - 1) >> 1;
+
+                if (compare(list[p], value) <= 0) break;
+                list[i] = list[p];
+                i = p;
+            }
+            list[i] = value;
+        }
+
+        public TValue Dequeue()
+        {
+            var min = list[0];
+            var x = list[list.Count - 1];
+            var i = 0;
+            while (true)
+            {
+                // Index of children.
+                var l = (i << 1) + 1;
+                var r = (i << 1) + 2;
+                if (l >= list.Count) break;
+
+                // Index of the smaller child.
+                var c = r < list.Count && compare(list[r], list[l]) < 0 ? r : l;
+
+                if (compare(list[c], x) >= 0) break;
+                list[i] = list[c];
+                i = c;
+            }
+            list[i] = x;
+            list.RemoveAt(list.Count - 1);
+            return min;
+        }
+
+        public BinaryHeap(List<TValue> list, Func<TValue, TValue, int> compare)
+        {
+            this.list = list;
+            this.compare = compare;
+        }
+    }
+
+    public static class BinaryHeap
+    {
+        public static BinaryHeap<X> Create<X>(Func<X, X, int> compare)
+        {
+            return new BinaryHeap<X>(new List<X>(), compare);
+        }
+
+        public static BinaryHeap<X> Create<X>()
+        {
+            return new BinaryHeap<X>(new List<X>(), Comparer<X>.Default.Compare);
+        }
+
+        public static BinaryHeap<X> FromEnumerable<X>(IEnumerable<X> xs, Func<X, X, int> compare)
+        {
+            var list = new List<X>(xs);
+            list.Sort(new Comparison<X>(compare));
+            return new BinaryHeap<X>(list, compare);
+        }
+
+        public static BinaryHeap<X> FromEnumerable<X>(IEnumerable<X> xs)
+        {
+            return FromEnumerable(xs, Comparer<X>.Default.Compare);
+        }
+    }
+}
