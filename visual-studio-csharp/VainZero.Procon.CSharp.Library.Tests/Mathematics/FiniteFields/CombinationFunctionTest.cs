c3cab85555fbccb85068e846f06168aeac1c3c37
vain0x <vainzerox@gmail.com>
2017-07-01 23:19:36 +0900

    Add CombinationFunction (for finite fields)

---
+++ visual-studio-csharp/VainZero.Procon.CSharp.Library.Tests/Mathematics/FiniteFields/CombinationFunctionTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace VainZero.Mathematics.FiniteFields
+{
+    public sealed class CombinationFunctionTest
+    {
+        [Fact]
+        public void Test()
+        {
+            var m = 6;
+            var mod = 7;
+
+            var expected =
+                new int[][]
+                {
+                    new[] { 1, 0, 0, 0, 0, 0, 0, },
+                    new[] { 1, 1, 0, 0, 0, 0, 0, },
+                    new[] { 1, 2, 1, 0, 0, 0, 0, },
+                    new[] { 1, 3, 3, 1, 0, 0, 0, },
+                    new[] { 1, 4, 6, 4, 1, 0, 0, },
+                    new[] { 1, 5, 3, 3, 5, 1, 0, },
+                    new[] { 1, 6, 1, 6, 1, 6, 1, },
+                };
+
+            var combinationFunction = new CombinationFunction(m, mod);
+            var actual =
+                Enumerable.Range(0, m + 1)
+                .Select(n =>
+                    Enumerable.Range(0, m + 1)
+                    .Select(k => (int)combinationFunction.Combination(n, k))
+                    .ToArray()
+                ).ToArray();
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ visual-studio-csharp/VainZero.Procon.CSharp.Library/Mathematics/FiniteFields/CombinationFunction.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VainZero.Mathematics.FiniteFields
+{
+    public sealed class CombinationFunction
+    {
+        struct PowerFunction
+        {
+            readonly int mod;
+
+            public long Power(long x, int n)
+            {
+                var y = 1L;
+                while (n > 0)
+                {
+                    if (n % 2 == 0)
+                    {
+                        x = (x * x) % mod;
+                        n /= 2;
+                    }
+                    else
+                    {
+                        y = (y * x) % mod;
+                        n--;
+                    }
+                }
+                return y;
+            }
+
+            public long Inverse(long x)
+            {
+                return Power(x, mod - 2);
+            }
+
+            public PowerFunction(int mod)
+            {
+                this.mod = mod;
+            }
+        }
+
+        struct FactorialFunction
+        {
+            readonly long[] dp;
+
+            public long Factorial(int n)
+            {
+                return dp[n];
+            }
+
+            public FactorialFunction(int m, int mod)
+            {
+                dp = new long[m + 1];
+
+                for (var i = 0; i <= m; i++)
+                {
+                    dp[i] = (i == 0 ? 1 : (dp[i - 1] * i) % mod);
+                }
+            }
+        }
+
+        readonly int mod;
+        readonly FactorialFunction factorial;
+        readonly long[] factorialInverse;
+
+        public long Combination(int n, int k)
+        {
+            if (k == 0 || n == k) return 1;
+            if (k < 0 || k > n) return 0;
+
+            var c = factorial.Factorial(n);
+            c = (c * factorialInverse[n - k]) % mod;
+            c = (c * factorialInverse[k]) % mod;
+            return c;
+        }
+
+        public CombinationFunction(int n, int mod)
+        {
+            this.mod = mod;
+            factorial = new FactorialFunction(n, mod);
+
+            var power = new PowerFunction(mod);
+            factorialInverse = new long[n + 1];
+            for (var i = 0; i < factorialInverse.Length; i++)
+            {
+                factorialInverse[i] = power.Inverse(factorial.Factorial(i));
+            }
+        }
+    }
+}
