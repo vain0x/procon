fcdea9632d09376c45113e1bb16057426e1376e6
vain0x <vainzerox@gmail.com>
2017-07-14 22:12:07 +0900

    Add PowerFunction (for finite fields)

---
+++ visual-studio-csharp/VainZero.Procon.CSharp.Library.Tests/Mathematics/FiniteFields/PowerFunctionTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace VainZero.Mathematics.FiniteFields
+{
+    public sealed class PowerFunctionTest
+    {
+        const int Mod = 1000000007;
+        readonly PowerFunction f = new PowerFunction(Mod);
+
+        [Fact]
+        public void Test_Power()
+        {
+            Assert.Equal(0, f.Power(0, 1));
+            Assert.Equal(1, f.Power(0, 0));
+            Assert.Equal(1, f.Power(1, 99));
+            Assert.Equal(16, f.Power(2, 4));
+            Assert.Equal(243, f.Power(3, 5));
+
+            Assert.Equal(959898892, f.Power(314159, 271828));
+            Assert.Equal(597324669, f.Power(271828, 314159));
+        }
+
+        [Theory]
+        [InlineData(1L)]
+        [InlineData(2L)]
+        [InlineData(3L)]
+        [InlineData(314159L)]
+        [InlineData(1000000006L)]
+        public void Test_Inverse(long value)
+        {
+            Assert.Equal(1, f.Inverse(value) * value % Mod);
+        }
+    }
+}
---
+++ visual-studio-csharp/VainZero.Procon.CSharp.Library/Mathematics/FiniteFields/PowerFunction.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VainZero.Mathematics.FiniteFields
+{
+    public sealed class PowerFunction
+    {
+        readonly int mod;
+
+        public long Power(long x, int n)
+        {
+            var y = 1L;
+            while (n > 0)
+            {
+                if (n % 2 == 0)
+                {
+                    x = (x * x) % mod;
+                    n /= 2;
+                }
+                else
+                {
+                    y = (y * x) % mod;
+                    n--;
+                }
+            }
+            return y;
+        }
+
+        public long Inverse(long x)
+        {
+            return Power(x, mod - 2);
+        }
+
+        public PowerFunction(int mod)
+        {
+            this.mod = mod;
+        }
+    }
+}
