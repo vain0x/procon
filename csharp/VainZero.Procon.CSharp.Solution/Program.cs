81fe47af3ae7b815c84ef850adb97ae30cfa3234
vain0x <vainzerox@gmail.com>
2017-11-05 01:58:28 +0900

    Add C# app and library for .NET Core 2.0

---
+++ csharp/VainZero.Procon.CSharp.Solution/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VainZero.Procon.CSharp.Solution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
