1067b8ca322952ca86ab4be316bbcae336ecf75b
vain0x <vainzerox@gmail.com>
2018-05-27 01:12:02 +0900

    Collapse procon mod

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,8 +1,7 @@
 #![allow(unused_imports)]
 #![allow(non_snake_case)]
 
-use procon::*;
-use std::cmp::{max, min};
+use std::cmp::{max, min, Ordering};
 use std::collections::*;
 use std::ops::*;
 use std::*;
@@ -21,45 +20,45 @@ mod tests {
     }
 }
 
-pub mod procon {
-    use std;
-    use std::cmp::*;
-    use std::collections::*;
-    use std::io;
-    use std::mem;
-    use std::str::FromStr;
+// -----------------------------------------------
+// Framework
+// -----------------------------------------------
 
-    pub fn rl() -> String {
-        let mut buf = String::new();
-        io::stdin().read_line(&mut buf).unwrap();
-        buf.trim_right().to_owned()
+#[allow(unused)]
+fn rl() -> String {
+    let mut buf = String::new();
+    io::stdin().read_line(&mut buf).unwrap();
+    buf.trim_right().to_owned()
+}
+
+#[allow(unused)]
+fn rw<T>() -> Vec<T>
+where
+    T: std::str::FromStr,
+    T::Err: std::fmt::Debug,
+{
+    let mut buf = String::new();
+    io::stdin().read_line(&mut buf).unwrap();
+    buf.split_whitespace()
+        .map(|word| T::from_str(word).unwrap())
+        .collect()
+}
+
+// -----------------------------------------------
+// Polyfill
+// -----------------------------------------------
+
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct Rev<T>(pub T);
+
+impl<T: PartialOrd> PartialOrd for Rev<T> {
+    fn partial_cmp(&self, other: &Rev<T>) -> Option<Ordering> {
+        other.0.partial_cmp(&self.0)
     }
+}
 
-    pub fn rw<T>() -> Vec<T>
-    where
-        T: std::str::FromStr,
-        T::Err: std::fmt::Debug,
-    {
-        let mut buf = String::new();
-        io::stdin().read_line(&mut buf).unwrap();
-        buf.split_whitespace()
-            .map(|word| T::from_str(word).unwrap())
-            .collect()
-    }
-
-    // Polyfill
-    #[derive(PartialEq, Eq, Clone, Debug)]
-    pub struct Rev<T>(pub T);
-
-    impl<T: PartialOrd> PartialOrd for Rev<T> {
-        fn partial_cmp(&self, other: &Rev<T>) -> Option<Ordering> {
-            other.0.partial_cmp(&self.0)
-        }
-    }
-
-    impl<T: Ord> Ord for Rev<T> {
-        fn cmp(&self, other: &Rev<T>) -> Ordering {
-            other.0.cmp(&self.0)
-        }
+impl<T: Ord> Ord for Rev<T> {
+    fn cmp(&self, other: &Rev<T>) -> Ordering {
+        other.0.cmp(&self.0)
     }
 }
