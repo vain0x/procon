346b2c3621c0df178a6542b9d8c251bae03121e5
vain0x <vainzerox@gmail.com>
2018-06-14 21:16:44 +0900

    Move Rev to lib

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -76,21 +76,6 @@ impl<'a> std::fmt::Debug for DebugMap<'a> {
 // Polyfill
 // -----------------------------------------------
 
-#[derive(PartialEq, Eq, Clone, Debug)]
-pub struct Rev<T>(pub T);
-
-impl<T: PartialOrd> PartialOrd for Rev<T> {
-    fn partial_cmp(&self, other: &Rev<T>) -> Option<Ordering> {
-        other.0.partial_cmp(&self.0)
-    }
-}
-
-impl<T: Ord> Ord for Rev<T> {
-    fn cmp(&self, other: &Rev<T>) -> Ordering {
-        other.0.cmp(&self.0)
-    }
-}
-
 #[allow(unused)]
 macro_rules! eprintln {
     ($($arg:expr),*) => { _eprintln(format_args!($($arg),*)) }
