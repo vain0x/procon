39ff0dde7701c53bc7741c2c1c6fe595e7de362e
vain0x <vainzerox@gmail.com>
2018-12-06 23:06:26 +0900

    feat: Add 'chinese_reminder' function

--- rust/src/procon/gcd.rs
+++ rust/src/procon/gcd.rs
@@ -21,9 +21,29 @@ pub fn lcm(x: i64, y: i64) -> i64 {
     x / gcd(x, y) * y
 }
 
+/// Solves the following simultaneous equations:
+///     x ≡ a1 (mod n1),
+///     x ≡ a2 (mod n2)
+/// to get `(x, m)`
+/// where m = `lcm(n1, n2)`
+/// and x is the unique solution in range `0 <= x < m`.
+/// Based on Chinese Reminder Theorem.
+/// To reduce, use `Some((0, 1))` as unit.
+pub fn chinese_reminder(a1: i64, n1: i64, a2: i64, n2: i64) -> Option<(i64, i64)> {
+    let (d, x, _) = gcd_ext(n1, n2);
+    if (a2 - a1).abs() % d != 0 {
+        return None;
+    }
+
+    let m = n1 * (n2 / d);
+    let t = (a2 - a1) / d * x % (n2 / d);
+    let u = ((a1 + n1 * t) % m + m) % m;
+    Some((u, m))
+}
+
 #[cfg(test)]
 mod tests {
-    use super::{gcd, gcd_ext, lcm};
+    use super::{chinese_reminder, gcd, gcd_ext, lcm};
 
     #[test]
     fn test_gcd() {
@@ -59,5 +79,16 @@ mod tests {
     fn test_lcm() {
         assert_eq!(lcm(1, 1), 1);
         assert_eq!(lcm(12, 18), 36);
+    }
+
+    #[test]
+    fn test_chinese_reminder() {
+        assert_eq!(
+            vec![(2, 3), (3, 5), (2, 7)]
+                .into_iter()
+                .fold(Some((0, 1)), |acc, (y, m)| acc
+                    .and_then(|(x, n)| chinese_reminder(x, n, y, m))),
+            Some((23, 105))
+        );
     }
 }
