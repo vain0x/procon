aca25e7acda3806f8f45038d84b8d4fcebd0f2b3
vain0x <vainzerox@gmail.com>
2020-04-15 19:06:20 +0900

    feat: 標準入力を受け取るための Scan 型を追加

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -6,18 +6,39 @@
 use std::collections::*;
 use std::ops::*;
 
+struct StdinLines<R>(String, R);
+
+impl<R: std::io::BufRead> Iterator for StdinLines<R> {
+    type Item = std::str::SplitWhitespace<'static>;
+
+    fn next(&mut self) -> Option<std::str::SplitWhitespace<'static>> {
+        self.0.clear();
+        std::io::BufRead::read_line(&mut self.1, &mut self.0).unwrap();
+        Some(Box::leak(self.0.clone().into_boxed_str()).split_whitespace())
+    }
+}
+
+pub struct Scan<'a>(std::iter::Flatten<StdinLines<std::io::StdinLock<'a>>>);
+
+impl<'a> Scan<'a> {
+    fn new(stdin: std::io::StdinLock<'a>) -> Self {
+        Scan(StdinLines(String::new(), stdin).flatten())
+    }
+
+    pub fn word<T: std::str::FromStr>(&mut self) -> T {
+        self.0.next().unwrap().parse().ok().unwrap()
+    }
+
+    pub fn list<T: std::str::FromStr>(&mut self, len: usize) -> Vec<T> {
+        (0..len).map(|_| self.word()).collect()
+    }
+}
+
 fn main() {
     let stdin = std::io::stdin();
-    let mut stdin = stdin.lock();
-    let mut buf = String::new();
-    let mut words = std::iter::repeat_with(|| {
-        buf.clear();
-        std::io::BufRead::read_line(&mut stdin, &mut buf).unwrap();
-        Box::leak(buf.clone().into_boxed_str()).split_whitespace()
-    })
-    .flatten();
+    let mut scan = Scan::new(stdin.lock());
 
-    let N = words.next().unwrap().parse::<usize>().unwrap();
+    let N = scan.word::<usize>();
 
     println!("{}", N);
 }
