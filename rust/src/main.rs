8527b850fe52bd50d3afc60f192a477c7c6aa6fc
vain0x <vainzerox@gmail.com>
2020-06-07 20:51:46 +0900

    feat: 関数を削除して、スニペットに処理を埋め込む

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -7,25 +7,17 @@ use std::cmp::{max, min};
 use std::collections::*;
 use std::ops::*;
 
-fn read_line(reader: &mut impl std::io::BufRead, buf: &mut String) {
-    buf.clear();
-    reader.read_line(buf).unwrap();
-}
-
-fn parse<T: std::str::FromStr>(word: &str) -> T {
-    T::from_str(word).unwrap_or_else(|_| std::process::exit(66))
-}
-
-// ###############################################
-
 fn main() {
     let stdin = std::io::stdin();
     let mut stdin = stdin.lock();
     let mut buf = String::new();
 
-    read_line(&mut stdin, &mut buf);
-    let mut words = buf.split_whitespace();
-    let N: usize = parse(words.next().unwrap_or(""));
+    let mut words = {
+        buf.clear();
+        std::io::BufRead::read_line(&mut stdin, &mut buf).unwrap();
+        buf.split_whitespace()
+    };
+    let N: usize = words.next().unwrap().parse().unwrap();
 
     println!("{}", N);
 }
